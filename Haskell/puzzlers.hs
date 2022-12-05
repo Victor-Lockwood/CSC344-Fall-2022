cde8e69f1344a71128cf0cde97a307aae704935a
Victor Lockwood <vlockwoo@oswego.edu>
2022-12-05 16:30:18 -0500

    Task 6

--- Haskell/puzzlers.hs
+++ Haskell/puzzlers.hs
@@ -74,3 +74,33 @@ count e l = length [ x | x <- l, x == e]
 -- freqTable: Returns a list of ordered pairs of each element and the number of times they appear in a list
 
 freqTable list = [(e, count e list) | e <- list2set list]
+
+
+-------------------------------
+-- HIGHER ORDER FUNCTIONS
+-------------------------------
+
+
+-------------------------------
+-- tgl: Returns the triangular number for a given positive Int input
+
+tgl n = foldl (+) 0 [1..n]
+
+
+-------------------------------
+-- triangleSequence: Returns a list of triangular numbers from 1 to the given positive Int input
+
+triangleSequence n = map tgl [1..n]
+
+
+-------------------------------
+-- vowelCount: Returns the number of vowels that appear in a given list of lowercase letters
+
+vowelCount word = length $ filter (\x -> x `elem` "aeiou") word
+
+
+-------------------------------
+-- lcsim: Takes a function for mapping, a predicate for filtering and a list of elements.  
+--        Output is equivalent to that of [ f x | x <- xs, p x ]
+
+lcsim f p xs = map f (filter p xs)
