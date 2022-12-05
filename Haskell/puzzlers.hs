79bc7431e6a82ae0ec6df97ef189fb578562a7a6
Victor Lockwood <vlockwoo@oswego.edu>
2022-12-05 15:35:25 -0500

    Task 4

--- Haskell/puzzlers.hs
+++ Haskell/puzzlers.hs
@@ -20,3 +20,35 @@ averageWordLength wordString =
     / (fromIntegral (length wordList) )
     
     where wordList = words wordString
+
+
+-------------------------------
+-- list2set: Takes a list of objects with duplicates removed
+
+list2set [] = []
+list2set (x:xs) = 
+    if (x `elem` xs) 
+        then list2set xs
+    else x: list2set xs
+
+
+-------------------------------
+-- isPalindrome: Returns true if the input list is a palindrome
+
+isPalindrome [] = True
+isPalindrome [x] = True
+isPalindrome (x:xs) =
+    if (x == (last xs))
+    then isPalindrome (init xs) 
+    else False
+
+
+-------------------------------
+-- collatz: Returns the collatz sequence for a given positive integer
+
+collatz :: Integer -> [Integer]
+collatz 1 = [1]
+collatz x = 
+    if (odd x)
+    then x : collatz (3 * x + 1)
+    else x : collatz (div x 2)
